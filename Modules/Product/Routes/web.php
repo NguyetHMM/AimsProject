674dfc673de11a6f6bcd29489922655ae83ded29
trung-lha <62026316+trung-lha@users.noreply.github.com>
2020-12-13 19:03:21 +0700

    add show all Cds,Dvds

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -17,4 +17,6 @@ Route::prefix('product')->group(function() {
     // Route::get('/product-detail/{}','ProductController@processRequestDetail')->name('processRequestDetail');
     Route::get('/home','ProductController@home')->name('home');
     Route::get('show-book','ProductController@showBook')->name('showBook');
+    Route::get('show-cds','ProductController@showCDs')->name('showCDs');
+    Route::get('show-dvds','ProductController@showDVDs')->name('showDVDs');
 });
