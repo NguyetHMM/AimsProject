546f9d272a1dbb150124a225ca9da1955dc1e5ec
chuchichbong <doducthuan29@gmail.com>
2021-01-15 11:02:58 +0700

    edit

--- Modules/Payment/Resources/views/checkout.blade.php
+++ Modules/Payment/Resources/views/checkout.blade.php
@@ -35,9 +35,7 @@
 
 
     <!-- Modernizr JS -->
-    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> --}}
+
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
