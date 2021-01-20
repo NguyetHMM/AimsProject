700ef550de1e69ff6f8df6e0a56833b0111f22af
NguyetHMM <nguyet332747@gmail.com>
2021-01-20 15:01:26 +0700

    Update ProductController

--- Modules/Ship/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('ship::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('ship.name') !!}
-    </p>
-@endsection
